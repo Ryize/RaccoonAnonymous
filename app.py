32a3e5d36d400a392b070689852752276657162e
Чекашов Матвей Павлович <chekashovmatvey@gmail.com>
2022-04-19 23:11:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config['SECRET_KEY']: str = 'bgcegy3yg2d3ue2uwccuby2ubwcchjsbgvcwcuwbc2whbu11'
 app.config['SQLALCHEMY_DATABASE_URI']: str = 'sqlite:///bd.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']: bool = True
