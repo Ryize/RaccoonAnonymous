4215c735a84ac0e6bad57e70cd1505c3e15050d7
CMatvey <chekashovmatvey@gmail.com>
2022-04-19 22:35:54 +0300

    Add flask-sqlalchemy, requirements

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, flash, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
