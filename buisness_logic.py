4aa588e3f71541d5cd587bb6273c72a7196ef694
CMatvey <chekashovmatvey@gmail.com>
2022-05-02 12:23:32 +0300

    Refactoring. Added code docs

--- buisness_logic.py
+++ buisness_logic.py
@@ -10,6 +10,10 @@ from flask_socketio import SocketIO, join_room, leave_room, emit, send, rooms
 
 
 class MessageControl:
+    """
+    Класс, отвечающий за работу команд.
+    """
+
     def __init__(self, msg: str):
         self.msg = msg
         self._commands = {
@@ -22,7 +26,11 @@ class MessageControl:
             'unwarn': self.command_unwarn,
         }
 
-    def auto_command(self):
+    def auto_command(self) -> str:
+        """
+        Автоматически определяет и вызывает нужную команду. Команды записаны в self._commands.
+        :return: str (Текст, после выполнения команды)
+        """
         msg_split = self.msg.split()
         if len(msg_split) < 2:
             raise ValueError('Недостаточно аргументов у команды')
@@ -38,7 +46,15 @@ class MessageControl:
                 reason = ' '.join(msg_split[3:])
         return self._commands[msg_split[0][1:]](command=command, login=login, _time=_time, room=room, reason=reason)
 
-    def command_ban(self, login: str, _time: Optional[str] = None, reason: str = 'Не указанна!', *args, **kwargs):
+    def command_ban(self, login: str, _time: Optional[str] = None, reason: str = 'Не указанна!', *args,
+                    **kwargs) -> str:
+        """
+        Реализует логику команды блокировки (бана).
+        :param login: str (Логин пользователя, которого блокируют)
+        :param _time: str (Время блокировки)
+        :param reason: str (Причина блокировки)
+        :return: str (Информация - кто, кого, за что и на сколько заблокировал)
+        """
         dt_object = self.__convert_time(_time)
         user_ban = BanUser.query.filter_by(login=login, who_banned=current_user.id).first()
         if not user_ban:
@@ -51,7 +67,15 @@ class MessageControl:
         return f'<p style="color: #CD5C5C; font-size: 125%;">Пользователь {login} был заблокирован {time_ban} Администратором {current_user.name}. <br>Причина: <em>{reason}</em></p>'
 
     def command_rban(self, login: str, room: str, _time: Optional[str] = None, reason: str = 'Не указанна!', *args,
-                     **kwargs):
+                     **kwargs) -> str:
+        """
+        Реализуется логика блокировки пользователя в конкретной комнате.
+        :param login: str (Логин пользователя, которого блокируют)
+        :param room: str(Название комнаты)
+        :param _time: str (Время блокировки)
+        :param reason: str (Причина блокировки)
+        :return: str (Информация - кто, кого, за что и на сколько заблокировал)
+        """
         dt_object = self.__convert_time(_time)
         user = RoomBan.query.filter_by(login=login, room=room).first()
         if not user: user = RoomBan(login=login, room=room, reason=reason, ban_end_date=dt_object)
@@ -62,7 +86,14 @@ class MessageControl:
         if _time == '*': time_rban = f'навсегда'
         return f'<p style="color: #87CEEB; font-size: 125%;">Пользователь {login} был заблокирован в данной комнате {time_rban} Администратором {current_user.name}. <br>Причина: <em>{reason}</em></p>'
 
-    def command_mute(self, login: str, _time: str = None, reason: str = 'Не указанна!', *args, **kwargs):
+    def command_mute(self, login: str, _time: str = None, reason: str = 'Не указанна!', *args, **kwargs) -> str:
+        """
+        Реализует логику команды затыкания пользователя (мута, читать сообщения можно, писать нельзя).
+        :param login: str (Логин пользователя, которого блокируют)
+        :param _time: str (Время мута)
+        :param reason: str (Причина мута)
+        :return: str (Информация - кто, кого, за что и на сколько замутил)
+        """
         dt_object = self.__convert_time(_time)
 
         user_mute = MuteUser.query.filter_by(login=login, who_muted=current_user.id).first()
@@ -75,7 +106,12 @@ class MessageControl:
         if not _time: time_mute = f'навсегда'
         return f'<p style="color: #87CEEB; font-size: 115%;">Пользователь {login} был замучен {time_mute} Администратором {current_user.name}. <br>Причина: <em>{reason}</em></p>'
 
-    def command_warn(self, login: str, *args, **kwargs):
+    def command_warn(self, login: str, *args, **kwargs) -> str:
+        """
+        Реализует логику команды предупреждения (варна, после трёх предупреждения, пользователь блокируется на 45 минут).
+        :param login: str (Логин пользователя)
+        :return: str (Информация по предупреждению)
+        """
         user = User.query.filter_by(name=login).first()
         user.warn += 1
         msg = f'<label style="color: #B8860B; font-size: 125%;">Администратор {current_user.name} выдал {user.warn} предупреждение пользователю {login} '
@@ -87,7 +123,12 @@ class MessageControl:
         db.session.commit()
         return f'{msg}</label>'
 
-    def command_unban(self, login: str, *args, **kwargs):
+    def command_unban(self, login: str, *args, **kwargs) -> Optional[str]:
+        """
+        Реализует логику команды снятия блокировки (бана).
+        :param login: str (Логин пользователя, с которого снимают блокировку)
+        :return: str (Информация по разбану)
+        """
         user = BanUser.query.filter_by(login=login).first()
         if user:
             user.ban_time = self.__convert_time('1')
@@ -98,7 +139,12 @@ class MessageControl:
         msg = f'<label style="color: #B8860B; font-size: 125%;">Администратор {current_user.name} разбанил пользователя {login} '
         return f'{msg}</label>'
 
-    def command_unmute(self, login: str, *args, **kwargs):
+    def command_unmute(self, login: str, *args, **kwargs) -> Optional[str]:
+        """
+        Реализует логику команды снятия затыкания (мута).
+        :param login: str (Логин пользователя, с которого снимают мут)
+        :return: str (Информация по муту)
+        """
         user = MuteUser.query.filter_by(login=login).first()
         if user:
             user.mute_time = self.__convert_time('1')
@@ -109,7 +155,12 @@ class MessageControl:
         msg = f'<label style="color: #B8860B; font-size: 125%;">Администратор {current_user.name} размутил пользователя {login} '
         return f'{msg}</label>'
 
-    def command_unwarn(self, login: str, *args, **kwargs):
+    def command_unwarn(self, login: str, *args, **kwargs) -> Optional[str]:
+        """
+        Реализует логику команды снятия предупреждения (варна, предупреждения не уходят ниже 0).
+        :param login: str (Логин пользователя, с которого снимают варн)
+        :return: str (Информация по варну)
+        """
         user = User.query.filter_by(name=login).first()
         if user:
             if user.warn > 0:
@@ -121,7 +172,12 @@ class MessageControl:
         msg = f'<label style="color: #B8860B; font-size: 125%;">Администратор {current_user.name} снял одно предупреждение с пользователя {login}. Осталось {user.warn} предупреждений '
         return f'{msg}</label>'
 
-    def execute_admin_commands(self, message_id: int, room: str):
+    def execute_admin_commands(self, message_id: int, room: str) -> Optional[bool]:
+        """
+        Выполняет команду, если пользователь - администратор.
+        :return: Optional[bool] (None - пользователь не Администратор, True - команда успешно выполнена,
+                                False - что-то пошло не так, команда не выполнена)
+        """
         if User.query.filter_by(name=current_user.name).first().admin_status:
             if self._broadcast_command(message_id, room): return True
             try:
@@ -133,6 +189,12 @@ class MessageControl:
                 return False
 
     def _broadcast_command(self, message_id: int, room: str) -> bool:
+        """
+        Логика команды broadcast - отправляет сообщение во все комнаты.
+        :param message_id: int (id Сообщения)
+        :param room: str (Название комнаты)
+        :return: bool (True - сообщение успешно отправлено, False - введённая команда не broadcast или bc)
+        """
         if self.msg.split()[0][1:].lower() in ['broadcast', 'bc']:
             msg = self.msg.split()
             del msg[0]
@@ -143,7 +205,14 @@ class MessageControl:
             return True
         return False
 
-    def _get_cmd_answer(self, message_id: int, msg: str, room: str):
+    def _get_cmd_answer(self, message_id: int, msg: str, room: str) -> dict:
+        """
+        Реализует стандартный генератор ответа сервера.
+        :param message_id: int (id Сообщения)
+        :param msg: str (Текст сообщения)
+        :param room: str (Название комнаты)
+        :return: dict (Словарь со всем содержимым ответа(Например для: socketio.emit))
+        """
         data = MessageControl(msg.replace('\n', '')).auto_command()
         dict_template = {'id': message_id,
                          'msg': msg,
@@ -157,6 +226,12 @@ class MessageControl:
         return dict_template
 
     def _preparation_for_rban_command(self, room) -> str:
+        """
+        Приводит сообщение, к формату который поддерживает команда rban. Заменяет номер кормнаты через this,
+        на название комнаты.
+        :param room: str (Название комнаты)
+        :return: str (Новый текст сообщения)
+        """
         msg = self.msg
         if len(msg.split()) > 3 and msg.split()[0][1:].lower() == 'rban':
             msg = msg.split()
@@ -164,7 +239,18 @@ class MessageControl:
             msg = ' '.join(msg)
         return msg
 
-    def __convert_time(self, _time):
+    def __convert_time(self, _time: str) -> datetime:
+        """
+        Метод, для конвертации времени указанного в сообщении.
+        Просто число (Пример: 27) - секунды;
+        Добавляется m (Пример: 13m) - минуты (13 * 60);
+        Добавляется h (Пример: 18h) - часы (19 * 60 * 60);
+        Добавляется d (Пример: 34d) - дни (34 * 60 * 60 * 24);
+        Добавляется y (Пример: 2y) - года (2 * 60 * 60 * 24 * 365);
+        (Пустое значение или * - преобразуется в 700 лет)
+        :param _time: str (Текст с временем)
+        :return: datetime
+        """
         if not _time or _time == '*':
             _time = 60 * 60 * 24 * 365 * 700 + time.time()
         elif _time.find('m') != -1 or _time.find('M') != -1:
@@ -182,6 +268,11 @@ class MessageControl:
 
 
 def check_correct_data(message: dict) -> Optional[bool]:
+    """
+    Проверка корректности введённых данных. Не менее 1 и не более 1000 символов.
+    :param message: dict (Список, который нам предоставляет socketio)
+    :return: Optional[bool] (None - неверные данные, True - всё хорошо)
+    """
     msg = message['msg']
     if len(msg.replace(' ', '').replace('\n', '')) > 0 and len(msg) < 1000:
         return True
@@ -192,6 +283,12 @@ def check_correct_data(message: dict) -> Optional[bool]:
 
 
 def checking_possibility_sending_message(room: str, _time: datetime) -> bool:
+    """
+    Проверка, не заблокирован/замучен/заблокирован в данной комнате пользователь.
+    :param room: str (Название комнаты)
+    :param _time: datetime (Текущее время)
+    :return: bool (True - пользователь может отправлять сообщения, False - проверка/проверки не пройдены)
+    """
     room_ban = RoomBan.query.filter_by(login=current_user.name, room=room).first() or None
     user_ban = BanUser.query.filter_by(login=current_user.name).first() or None
     user_mute = MuteUser.query.filter_by(login=current_user.name).first() or None
@@ -205,6 +302,13 @@ def checking_possibility_sending_message(room: str, _time: datetime) -> bool:
 
 
 def get_ban_data(user_ban: BanUser, time_now: datetime, reason: str) -> Tuple[Optional[str], str]:
+    """
+    Получить время и причину блокировки.
+    :param user_ban: BanUser (Забаненный пользователь)
+    :param time_now: datetime (Текущее время)
+    :param reason: str (Причина)
+    :return: Tuple[Optional[str], str]
+    """
     ban_time = None
     if user_ban:
         ban_time = td_format(user_ban.ban_time - time_now)
@@ -214,6 +318,13 @@ def get_ban_data(user_ban: BanUser, time_now: datetime, reason: str) -> Tuple[Op
 
 
 def get_mute_data(user_mute: MuteUser, time_now: datetime, reason: str) -> Tuple[Optional[str], str]:
+    """
+    Получить время и причину мута.
+    :param user_mute: MuteUser (заткнутый пользователь)
+    :param time_now: datetime (Текущее время)
+    :param reason: str (Причина)
+    :return: Tuple[Optional[str], str]
+    """
     mute_time = None
     if user_mute:
         mute_time = td_format(user_mute.mute_time - time_now)
@@ -228,6 +339,11 @@ def get_mute_data(user_mute: MuteUser, time_now: datetime, reason: str) -> Tuple
 
 
 def td_format(td_object) -> str:
+    """
+    Привести от объекта datetime, к формату понятному человеку (Например: 5 лет 4 месяца 13 часов).
+    :param td_object: datetime (Объект который преобразовывается)
+    :return: str (Преобразованная дата)
+    """
     seconds = int(td_object.total_seconds())
     periods = [
         ('лет', 60 * 60 * 24 * 365),
@@ -248,6 +364,11 @@ def td_format(td_object) -> str:
 
 
 def complaint_on_message(msg: str) -> bool:
+    """
+    Команда жалобы на сообщение.
+    :param msg: str (Сообщение на которое пожаловались)
+    :return: bool (True - жалоба записанна в БД)
+    """
     msg_split = msg.split()
     if msg_split[0][1:].lower() != 'vote': return False
     login = current_user.name
